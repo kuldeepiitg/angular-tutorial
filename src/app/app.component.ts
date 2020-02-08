ead686648a200793551754eb8128acf4fafc6aaa
Kuldeep Yadav <kuldeepwrites@gmail.com>
2020-02-08 06:49:29 +0530

    4. Decoraters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+  descriptor.value();
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intro2angular';
+
+  constructor() {
+    this.simpleMethod();
+  }
+
+  @log
+  simpleMethod() {
+    console.log('hi there');
+  }
+
 }
